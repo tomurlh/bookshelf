29f06dd080e8eb7c64584c26f90ba54421968618
Thigo Medeiros <si-thiago@hotmail.com>
2018-07-30 02:54:51 -0300

    Implementing drag-n-drop of books

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './book.css';
+import React, { Component } from 'react'
+import _ from 'lodash'
+
+import logo from './logo.svg'
+
+import './App.css'
+import './defaults.css'
+import './book.css'
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
+
+import Book from './components/Book'
+
+import GridLayout from 'react-grid-layout'
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
+
+import * as BooksAPI from './utils/BooksAPI'
 
 class App extends Component {
+  state = {
+    books: []
+  }
+
+  generateLayout() {
+    const p = this.props
+    return this.state.books.map((book, i) => {
+      const y = _.result(p, 'y') || Math.ceil(Math.random() * 4) + 1
+      console.log((i * 5) % 12)
+      console.log(Math.floor(i / 5) * y);
+      return {
+        i: book.title,
+        x: (i * 5) % 12,
+        y: Math.floor(i / 5) * y,
+        w: 4,
+        h: 1
+      }
+    })
+  }
+
+
   render() {
-    let style = {
-      'margin-top': '50px'
-    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <GridLayout 
+          className="layout" 
+          cols={12} 
+          rowHeight={220} 
+          width={1200} 
+          layout={this.generateLayout()}
+          autoSize={true}
+          compactType={'horizontal'}
+          isResizable={false}>
+          {this.state.books.map((book) =>
+            <div key={book.title}>
+              <Book
+                title={book.title} 
+                cover={book.imageLinks.smallThumbnail} 
+                author={book.authors} />
+            </div>
+          )}
+        </GridLayout>
+
+
+        <footer className="footer">
+          <BottomNavigation>
+            <BottomNavigationAction
+              label="Recents"
+              icon={<AddCircleOutlineIcon />}
+            />
+          </BottomNavigation>
+        </footer>
       </div>
-    );
+    )
+  }
+
+
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 }
 
-export default App;
+export default App
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import ScheduleIcon from '@material-ui/icons/Schedule'
+import DoneIcon from '@material-ui/icons/Done'
+import DoneAllIcon from '@material-ui/icons/DoneAll'
 
-class App extends React.Component {
+class Book extends React.Component {
 	render() {
+		let iconStyle = { fontSize: 15, 'vertical-align': 'middle' }
 	    return (
 	        <div>
 	            <figure className='book'>
 
 	              <ul className='hardcover_front'>
 	                <li>
-	                  <img src={props.cover} alt="" width="100%" height="100%"/>
+	                  <img src={this.props.cover} alt="" width="100%" height="100%"/>
 	                </li>
 	                <li></li>
 	              </ul>
@@ -16,12 +20,13 @@ class App extends React.Component {
 	              <ul className='page'>
 	                <li></li>
 	                <li>
-	                    <div style={style}>
-	                      <a class="btn" href="#">Want to Read</a>
-	                      <br/><br/>
-	                      <a class="btn" href="#">Reading</a>
-	                      <br/><br/>
-	                      <a class="btn" href="#">Read</a>
+	                	<br/><br/>
+	                    <div>
+	                      <a className="btn" href="/">Want to Read <ScheduleIcon style={iconStyle} /></a>
+	                      <br/>
+	                      <a className="btn" href="/">Reading <DoneIcon style={iconStyle} /></a>
+	                      <br/>
+	                      <a className="btn" href="/">Read <DoneAllIcon style={iconStyle} /></a>
 	                    </div>
 	                </li>
 	                <li></li>
@@ -33,17 +38,21 @@ class App extends React.Component {
 	                <li></li>
 	                <li></li>
 	              </ul>
-	              <ul className='book_spine'>
-	                <li></li>
-	                <li></li>
-	              </ul>
+
 	              <figcaption>
-	                <h1>{props.title}</h1>
-	                <span>{props.author}</span>
-	                <p>{props.description}</p>
+	                <h2>{this.props.title}</h2>
+	                <span>{this.props.author}</span>
+	                <p>{this.props.description}</p>
 	              </figcaption>
 	            </figure>
 	        </div>
 	    )
 	}
+
+
+
+	componentDidMount() {
+	}
 }
+
+export default Book
