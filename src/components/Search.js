e5f35302046883ee00916fc5786a3de1c46dfcec
Thiago Medeiros <si-thiago@hotmail.com>
2018-08-01 15:39:22 -0300

    Removing unessential files

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import _ from 'lodash'
 import { Link } from 'react-router-dom'
 
 class Search extends React.Component {
@@ -9,22 +10,24 @@ class Search extends React.Component {
 				<div className="search-books-bar">
 					<Link to="/" className="close-search">Close</Link>
 					<div className="search-books-input-wrapper">
-					{/*
-					NOTES: The search from BooksAPI is limited to a particular set of search terms.
-					You can find these search terms here:
-					https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-					However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-					you don't find a specific author or title. Every search is limited by search terms.
-					*/}
-					<input type="text" placeholder="Search by title or author"/>
-
+					<input
+						type="text" 
+						placeholder="Search by title or author" 
+						onChange={_.debounce(this.queryBooks, 5000)} />
 				</div>
 			</div>
 				<p>Search view</p>
 			</div>
 		)
 	}
+
+
+
+	queryBooks() {
+		console.log('test')
+	}
+
+
 }
 
 export default Search
