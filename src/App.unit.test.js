bae16c71385f28ed6854e042d4fa4b17ed75cd7c
Thigo Medeiros <si-thiago@hotmail.com>
2018-08-24 03:22:36 -0300

    Removing not finished test

--- src/App.unit.test.js
+++ src/App.unit.test.js
@@ -35,30 +35,4 @@ describe('Components Rendering', () => {
 				description="something" />)
 		expect(wrapper.find('.btn')).to.have.length(4)
 	})
-
-
-
-	it('update Book shelf from currentlyReading to wantToRead', () => {
-		const wrapper = shallow(<App />);
-
-		wrapper.setState({
-			wantToRead: [],
-			currentlyReading: [{
-	            "title": "The Linux Command Line",
-	            "authors": [
-	                "William E. Shotts, Jr."
-	            ],
-	            "imageLinks": {
-	                "smallThumbnail": "",
-	                "thumbnail": ""
-	            },
-	            "id": "1",
-	            "shelf": "currentlyReading"
-        	}],
-			read: []
-		});
-		wrapper.find('btn').at(0).simulate('click');
-
-		expect(wrapper.state().wantToRead).to.have.length(1);
-	});
 })
